09fd880c216f29ef1459df2d4d52917092918848
Daniel Nguyen & Kathryn Ragudos <pair+dnguyensr.kragudos19@devbootcamp.com>
2016-06-08 14:38:04 -0700

    Update views, modify controlers and models

--- app/controllers/post.rb
+++ app/controllers/post.rb
@@ -1,8 +1,14 @@
-get '/posts' do
+get '/' do
   @posts = Post.all
   erb :'/posts/index'
 end
 
+get '/posts/:id' do
+  @post = Post.find(params[:id])
+
+  @user = User.find_by(id: @post.user_id)
+  erb :'/posts/show'
+end
 
 
 
