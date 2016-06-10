5040c0912063e829c100d616e94654e9b92db95f
Jesse Kinser & Jacqueline Phan <pair+jdkinser.jtphan14@devbootcamp.com>
2016-06-09 19:22:34 -0700

    Add comment ajax append function

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -1,15 +1,9 @@
 post '/posts/:id/comments' do
-	post = Post.find(params[:id])
-	comment = post.comments.create(content: params[:content], user: current_user, commentable: post)
-	puts comment.to_json
+	@post = Post.find(params[:id])
+	comment = @post.comments.create(content: params[:content], user: current_user, commentable: @post)
 	if request.xhr?
 		content_type :json
-		# {content: params[:content], user_id: current_user.id, commentable_id: post.id, commentable_type: post  }.to_json
-		# comment_text = comment.content
 		comment.to_json
-		# puts (json_response = { comment: comment, comment_text: comment_text }.to_json)
-		# json_response
-
 	else
 		redirect "/posts/#{params[:id]}"
 	end
--- public/js/application.js
+++ public/js/application.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
 ////////////////////////////Add Comment/////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
   $('#new_comment').hide();
+    $('input[value="Add a Comment"]').on('click',(function(event){
+       event.preventDefault();
+       $('#new_comment').show();
+      $(this).hide();
+    }));
 
   $('.post-container').on('submit','#new_comment', function(e){
     e.preventDefault();
@@ -12,7 +17,8 @@ $(document).ready(function() {
       data: $(this).serialize()
     })
     .done(function(response){
-      alert('florb');
+      $('#all-post-comments').append(response.content);
+      // $('#all-post-comments')[0].reset();
     }).fail(function(){
       alert('fail');
     });
@@ -34,16 +40,16 @@ $(document).ready(function() {
   //   }
   // });
 
-  $('input[value="Add Answer"]').on("click", function(e){
-    e.preventDefault();
-    // var answer = $("#answer").val();
-    // var uri = $(this).closest('form').attr('action');
-    // var ajaxRequest = $.post(url, $(this).serialize());
-    // console.log(ajaxRequest);
-    // $.ajax({
-      url: uri
-      type: "POST"
-      dataType: "json"
-    })
-    $("#bigcontainer").append("<h3>Testing</h3>")
+//   $('input[value="Add Answer"]').on("click", function(e){
+//     e.preventDefault();
+//     var answer = $("#answer").val();
+//     var uri = $(this).closest('form').attr('action');
+//     var ajaxRequest = $.post(url, $(this).serialize());
+//     console.log(ajaxRequest);
+//     $.ajax({
+//       url: uri
+//       type: "POST"
+//       dataType: "json"
+//     })
+//     $("#bigcontainer").append("<h3>Testing</h3>")
 });
